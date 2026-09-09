c9db86a2230336c8698b2cdb9aa702303ca5d069
Chratzon <chris.atze.ca@gmail.com>
2026-09-10 00:18:55 +0200

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Roadcase Service Worker – App-Shell offline, Daten liegen in IndexedDB */
-const CACHE = 'roadcase-v1';
+const CACHE = 'roadcase-v2';
 const SHELL = [
   './', './index.html', './styles.css', './app.js',
   './manifest.webmanifest', './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png'
